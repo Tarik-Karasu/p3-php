a44be502cb8fb3fb711e9d3887fe0a63475034ac
Tarik-Karasu <9022679@student.zadkine.nl>
2026-03-05 13:03:47 +0100

    opdracht 1

---
+++ shorthand.php
@@ -0,0 +1,21 @@
+<?php
+$naam = "Tarik Emre Karasu";
+$leeftijd = 19;
+$programmeertaal = "Python";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Shorthand Voorbeeld</title>
+</head>
+<body>
+
+<h1>Mijn gegevens</h1>
+
+<p>Naam: <?= $naam ?></p>
+<p>Leeftijd: <?= $leeftijd ?></p>
+<p>Favoriete programmeertaal: <?= $programmeertaal ?></p>
+
+</body>
+</html>
